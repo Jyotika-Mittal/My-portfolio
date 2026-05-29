4c6653d19480a41f533f18f5a9e3c68ac97a1dfe
Jyotika Mittal <jmittal_be23@thapar.edu>
2026-05-29 19:49:16 +0530

    projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -25,10 +25,10 @@ function Projects() {
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="Medimentor"
+              description="Medimentor is an AI-powered medical learning platform that helps students improve surgical knowledge and practical skills through an intelligent medical chatbot, performance tracking, and an immersive VR surgery simulator. Built with React, FastAPI, OpenAI GPT, and Unity WebGL, it provides an interactive and personalized learning experience."
+              ghLink="https://github.com/Jyotika-Mittal/VR-simmulations"
+              demoLink="https://final-med.vercel.app"
             />
           </Col>
 
